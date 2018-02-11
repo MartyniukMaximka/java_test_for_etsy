963a8f1eabb93164639dae16aa7f831478b9cacf
MartyniukMaximka <martyniuk.maxim@gmail.com>
2018-02-11 21:49:51 +0200

    1

---
+++ src/main/java/Hellpers.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hellpers {
+    FirefoxDriver wd;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+    }
+
+    public void likeProduct(String url) {
+        wd.get(url);
+        for (int i = 0; i <= 56; i++) {
+            wd.findElement(By.xpath(".//*[@class='etsy-icon not-favorited position-absolute text-white']")).click();
+        }
+    }
+
+    public void login() {
+        wd.get("https://www.etsy.com/");
+        wd.findElement(By.xpath(".//*[@id='sign-in']")).click();
+        WebDriverWait wait = new WebDriverWait(wd, 10);
+        WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.id("username-existing")));
+        wd.findElement(By.id("username-existing")).click();
+        wd.findElement(By.id("username-existing")).clear();
+        wd.findElement(By.id("username-existing")).sendKeys("olgastarko");
+        wd.findElement(By.id("password-existing")).click();
+        wd.findElement(By.id("password-existing")).clear();
+        wd.findElement(By.id("password-existing")).sendKeys("olgastarko123");
+        wd.findElement(By.id("signin-button")).click();
+        WebDriverWait wait1 = new WebDriverWait(wd, 10);
+        WebElement sucksesLogin = wait.until(ExpectedConditions.elementToBeClickable(By.id("gnav-account-favorites-description")));
+    }
+}
---
+++ src/test/tests/Etsy.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Etsy extends Hellpers{
+
+
+    @Test
+    public void Etsy()
+    {
+        login();
+        likeProduct("https://www.etsy.com/c/toys-and-games/toys/baby-and-toddler-toys");
+        likeProduct("https://www.etsy.com/c/toys-and-games/toys/baby-and-toddler-toys?explicit=1&page=2&use_mmx=1");
+
+    }
+
+    
+}
